67239e5434fa0e07e8a51806d8a2ac11a36a7938
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-07-10 18:09:29 +0100

    cpp06 - ex00 - setup

---
+++ ex00/inc/ScalarConverter.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/10 17:54:44 by lude-bri          #+#    #+#             */
+/*   Updated: 2025/07/10 17:59:26 by lude-bri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class ScalarConverter {
+
+	//will contain only one static method "convert"
+	//that will take as a parameter a string representation of a c++ literal
+	//in its most common form and output its value in the following series
+	//of scalar types
+};
